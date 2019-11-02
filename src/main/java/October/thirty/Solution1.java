7a7c1c5d0a6e863d15486421420283f3418f70de
zhtttylz <2026211008@qq.com>
2019-11-03 03:25:46 +0800

    11. Container With Most Water(last)

--- src/main/java/October/thirty/Solution1.java
+++ src/main/java/October/thirty/Solution1.java
@@ -3,7 +3,8 @@ package October.thirty;
 import org.omg.CORBA.RepositoryIdHelper;
 
 /**11. Container With Most Water
- * 使用双指针的方式
+ * 使用双指针的方式,其实并不用将所有可能性列出来,因为是面积是最小边*长度
+ * 所以只要不停的跳过最小边同时*两个边之间的距离即为当前最大面积
  * @author zhtttylz
  * @date 2019/11/3 3:00
  */
@@ -30,7 +31,7 @@ public class Solution1 {
             if(height[left] < height[right]){
 
                 left ++;
-            }else{// 如果left > right了 说明right已经和left之前的所有比它小的边组成过矩形了
+            }else{
 
                 right--;
             }
