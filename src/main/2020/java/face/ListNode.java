69b1701df5bb7244fea2bd3a70e3b05dea16a926
zhtttylz <2026211008@qq.com>
2020-06-21 12:02:05 +0800

    ListNode class

---
+++ src/main/2020/java/face/ListNode.java
@@ -0,0 +1,14 @@
+package face;
+
+/**
+ * @author zhtttylz
+ * @date 2020/6/7 12:07
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
