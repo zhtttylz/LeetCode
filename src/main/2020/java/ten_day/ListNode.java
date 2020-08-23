e49dacd3f679752edeafbb6901ba0c5bbd204ace
zhtttylz <2026211008@qq.com>
2020-08-23 10:09:08 +0800

    add class ListNode

---
+++ src/main/2020/java/ten_day/ListNode.java
@@ -0,0 +1,23 @@
+package ten_day;
+
+/**
+ * @author zhtttylz
+ * @date 2020/8/23 10:08
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
