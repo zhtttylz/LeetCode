cf24965b8f0e4f629b47a0cbabcb94620ed59496
zhanghualong <zhanghualong@qianxin.com>
2020-11-06 01:19:56 +0800

    121. Best Time to Buy and Sell Stock

---
+++ src/main/2020/java/face/BestTimetoBuyandSellStock.java
@@ -0,0 +1,45 @@
+package face;
+
+/**
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ *
+ * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ *
+ * Note that you cannot sell a stock before you buy one.
+ *
+ * Example 1:
+ *
+ * Input: [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ *              Not 7-1 = 6, as selling price needs to be larger than buying price.
+ * Example 2:
+ *
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ *
+ * 因为只能进行一次买卖，所以找出全局最大值和全局最小值即可
+ */
+public class BestTimetoBuyandSellStock {
+
+    public int maxProfit(int[] prices) {
+
+        if(prices == null || prices.length == 0) return 0;
+
+        // 全局最大自值
+        int res = 0;
+
+        // 全局最小值
+        int tmp = prices[0];
+
+        for(int i =0; i < prices.length; i++){
+
+            tmp = Math.min(tmp, prices[i]);
+            res = Math.max(res, prices[i] - tmp);
+
+        }
+
+        return res;
+    }
+}
