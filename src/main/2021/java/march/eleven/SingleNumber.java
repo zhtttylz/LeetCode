5694a46697f09852aaa8b5d79d83a8ee885948c3
zhtttylz <hualong.z@hotmail.com>
2021-03-11 21:50:41 +0800

    136 Single Number 小知识：n^n=0  0^a=a

---
+++ src/main/2021/java/march/eleven/SingleNumber.java
@@ -0,0 +1,15 @@
+package march.eleven;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+
+        int res = nums[0];
+        for(int i = 1; i < nums.length; i++) res ^= nums[i];
+
+        return res;
+    }
+
+}
