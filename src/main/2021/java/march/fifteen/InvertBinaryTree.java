a3231de6de252db8b7ce6543e80d98970a916ffb
zhtttylz <hualong.z@hotmail.com>
2021-03-15 22:09:21 +0800

    226 Invert Binary Tree 镜像二叉树

---
+++ src/main/2021/java/march/fifteen/InvertBinaryTree.java
@@ -0,0 +1,30 @@
+package march.fifteen;
+
+public class InvertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root) {
+
+        if(root == null) return null;
+
+        // 左右互换，镜像二叉树
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
