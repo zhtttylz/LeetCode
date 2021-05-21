7cbff94f26366f8b86aee8256d954854c461b81f
zhtttylz <hualong.z@hotmail.com>
2021-05-21 15:53:04 +0800

    146. LRU Cache 使用自定义的linkList进行处理(后续进行复习)

---
+++ src/main/2021/java/bak/LUR缓存机制/LRUCache.java
@@ -0,0 +1,18 @@
+package bak.LUR缓存机制;
+
+
+class LRUCache {
+
+    public LRUCache(int capacity) {
+
+    }
+
+    public int get(int key) {
+
+        return 0;
+    }
+
+    public void put(int key, int value) {
+
+    }
+}
