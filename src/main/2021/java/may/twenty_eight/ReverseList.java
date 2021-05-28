05276b3c2529547ba2c04ba5c61dec54c8d8f9b7
zhtttylz <hualong.z@hotmail.com>
2021-05-28 10:54:34 +0800

    206. 反转链表 递归解决

---
+++ src/main/2021/java/may/twenty_eight/ReverseList.java
@@ -0,0 +1,22 @@
+package may.twenty_eight;
+
+public class ReverseList {
+
+    public ListNode reverseList(ListNode head) {
+
+        if(head == null || head.next == null) return head;
+        ListNode res = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return res;
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
